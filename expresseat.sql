7fb70b8ed68e0ea3aa4b11f8ffd6dcc53ceff8e5
Aulinee <audreyduyan1@gmail.com>
2021-01-09 07:35:27 +0800

    database bug remove

--- expresseat.sql
+++ expresseat.sql
@@ -1,141 +1,475 @@
-CREATE TABLE Customer (
-  ID         int(10) NOT NULL, 
-  Username   varchar(255) NOT NULL, 
-  Email      varchar(255) NOT NULL, 
-  Password   varchar(25) NOT NULL, 
-  FirstName  varchar(255) NOT NULL, 
-  LastName   varchar(255) NOT NULL, 
-  Gender     varchar(255) NOT NULL, 
-  Phone      int(11) NOT NULL, 
-  Address1   varchar(255) NOT NULL, 
-  Address2   varchar(255) NOT NULL, 
-  City       varchar(255) NOT NULL, 
-  State      varchar(255) NOT NULL, 
-  Country    varchar(255) NOT NULL, 
-  PostalCode int(10) NOT NULL, 
-  PRIMARY KEY (ID));
-  
-CREATE TABLE Review (
-  ID         int(10) NOT NULL AUTO_INCREMENT, 
-  CustomerID int(10) NOT NULL, 
-  Comment    varchar(1000) NOT NULL, 
-  Rating     int(1) NOT NULL, 
-  PRIMARY KEY (ID));
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 09, 2021 at 12:34 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
-CREATE TABLE Order (
-  ID                        int(10) NOT NULL AUTO_INCREMENT, 
-  CustomerID                int(10) NOT NULL, 
-  OrderTime                 time(7) NOT NULL, 
-  OrderDate                 date NOT NULL, 
-  Shopping_CartOrder_ItemID int(10) NOT NULL, 
-  PRIMARY KEY (ID));
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE Item (
-  Code            int(10) NOT NULL AUTO_INCREMENT, 
-  Name            varchar(255) NOT NULL, 
-  ItemDescription varchar(255) NOT NULL, 
-  QtyInStock      int(10) NOT NULL, 
-  BuyPrice        numeric(19, 0) NOT NULL, 
-  PRIMARY KEY (Code));
 
-CREATE TABLE Menu (
-  ItemCode int(10) NOT NULL);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE Payment (
-  CheckNum    varchar(255) NOT NULL, 
-  CustomerID  int(10) NOT NULL, 
-  PaymentDate date NOT NULL, 
-  Amount      numeric(19, 0) NOT NULL, 
-  PRIMARY KEY (CheckNum));
+--
+-- Database: `expresseat`
+--
 
-CREATE TABLE Receipt (
-  Receipt         int(10) NOT NULL AUTO_INCREMENT, 
-  CustomerID      int(10) NOT NULL, 
-  PaymentCheckNum varchar(255) NOT NULL, 
-  OrderID         int(10) NOT NULL, 
-  GenerateDate    date NOT NULL, 
-  PRIMARY KEY (Receipt));
+-- --------------------------------------------------------
 
-CREATE TABLE Order_List (
-  OrderQueue  int(10) NOT NULL AUTO_INCREMENT, 
-  OrderID     int(10) NOT NULL, 
-  OrderStatus varchar(255) NOT NULL, 
-  PRIMARY KEY (OrderQueue));
+--
+-- Table structure for table `administrator`
+--
 
-CREATE TABLE Order_Item (
-  ID        int(10) NOT NULL AUTO_INCREMENT, 
-  ItemCode  int(10) NOT NULL, 
-  Qty       int(10) NOT NULL, 
-  PriceEach numeric(19, 0) NOT NULL, 
-  PRIMARY KEY (ID));
+CREATE TABLE `administrator` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `StoreCode` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Shopping_Cart (
-  Order_ItemID int(10) NOT NULL, 
-  PRIMARY KEY (Order_ItemID));
+-- --------------------------------------------------------
 
-CREATE TABLE Delivery (
-  ID                   int(10) NOT NULL AUTO_INCREMENT, 
-  Order_ListOrderQueue int(10) NOT NULL, 
-  DeliveryDate         date NOT NULL, 
-  DeliveryTime         time(7) NOT NULL, 
-  DeliveryStatus       varchar(255) NOT NULL, 
-  PRIMARY KEY (ID));
+--
+-- Table structure for table `customer`
+--
 
-CREATE TABLE Staff (
-  ID                   int(10) NOT NULL AUTO_INCREMENT, 
-  FirstName            varchar(255) NOT NULL, 
-  LastName             varchar(255) NOT NULL, 
-  JobTitle             varchar(100) NOT NULL, 
-  Email                varchar(255) NOT NULL, 
-  Username             varchar(255) NOT NULL, 
-  Password             varchar(25) NOT NULL, 
-  Order_ListOrderQueue int(10) NOT NULL, 
-  StoreCode            int(10) NOT NULL, 
-  PRIMARY KEY (ID));
+CREATE TABLE `customer` (
+  `ID` int(10) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `Gender` varchar(255) NOT NULL,
+  `Phone` int(11) NOT NULL,
+  `Address1` varchar(255) NOT NULL,
+  `Address2` varchar(255) NOT NULL,
+  `City` varchar(255) NOT NULL,
+  `State` varchar(255) NOT NULL,
+  `Country` varchar(255) NOT NULL,
+  `PostalCode` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE DeliveryMan (
-  DeliveryID int(10) NOT NULL, 
-  FirstName  varchar(255) NOT NULL, 
-  LastName   varchar(255) NOT NULL, 
-  Username   varchar(255) NOT NULL, 
-  Password   varchar(25) NOT NULL, 
-  Email      varchar(255) NOT NULL, 
-  Phone      int(11) NOT NULL);
+-- --------------------------------------------------------
 
-CREATE TABLE Store (
-  Code       int(10) NOT NULL AUTO_INCREMENT, 
-  City       varchar(255) NOT NULL, 
-  Phone      int(11) NOT NULL, 
-  Address1   varchar(255) NOT NULL, 
-  Address2   varchar(255) NOT NULL, 
-  State      varchar(255) NOT NULL, 
-  Country    varchar(255) NOT NULL, 
-  PostalCode int(10) NOT NULL, 
-  PRIMARY KEY (Code));
+--
+-- Table structure for table `delivery`
+--
 
-CREATE TABLE Administrator (
-  ID        int(10) NOT NULL AUTO_INCREMENT, 
-  FirstName varchar(255) NOT NULL, 
-  LastName  varchar(255) NOT NULL, 
-  Email     varchar(255) NOT NULL, 
-  Username  varchar(255) NOT NULL, 
-  Password  varchar(25) NOT NULL, 
-  StoreCode int(10) NOT NULL, 
-  PRIMARY KEY (ID));
+CREATE TABLE `delivery` (
+  `ID` int(10) NOT NULL,
+  `Order_ListOrderQueue` int(10) NOT NULL,
+  `DeliveryDate` date NOT NULL,
+  `DeliveryTime` time(6) NOT NULL,
+  `DeliveryStatus` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE Order ADD CONSTRAINT FKOrder556711 FOREIGN KEY (CustomerID) REFERENCES Customer (ID);
-ALTER TABLE Menu ADD CONSTRAINT FKMenu452447 FOREIGN KEY (ItemCode) REFERENCES Item (Code);
-ALTER TABLE Payment ADD CONSTRAINT FKPayment75777 FOREIGN KEY (CustomerID) REFERENCES Customer (ID);
-ALTER TABLE Receipt ADD CONSTRAINT FKReceipt921862 FOREIGN KEY (CustomerID) REFERENCES Customer (ID);
-ALTER TABLE Receipt ADD CONSTRAINT FKReceipt156158 FOREIGN KEY (PaymentCheckNum) REFERENCES Payment (CheckNum);
-ALTER TABLE Receipt ADD CONSTRAINT FKReceipt25420 FOREIGN KEY (OrderID) REFERENCES Order (ID);
-ALTER TABLE Review ADD CONSTRAINT FKReview380693 FOREIGN KEY (CustomerID) REFERENCES Customer (ID);
-ALTER TABLE Order_List ADD CONSTRAINT FKOrder_List286387 FOREIGN KEY (OrderID) REFERENCES Order (ID);
-ALTER TABLE Order_Item ADD CONSTRAINT FKOrder_Item307080 FOREIGN KEY (ItemCode) REFERENCES Item (Code);
-ALTER TABLE Shopping_Cart ADD CONSTRAINT FKShopping_C101954 FOREIGN KEY (Order_ItemID) REFERENCES Order_Item (ID);
-ALTER TABLE Order ADD CONSTRAINT FKOrder625383 FOREIGN KEY (Shopping_CartOrder_ItemID) REFERENCES Shopping_Cart (Order_ItemID);
-ALTER TABLE Delivery ADD CONSTRAINT FKDelivery790234 FOREIGN KEY (Order_ListOrderQueue) REFERENCES Order_List (OrderQueue);
-ALTER TABLE Staff ADD CONSTRAINT FKStaff882782 FOREIGN KEY (Order_ListOrderQueue) REFERENCES Order_List (OrderQueue);
-ALTER TABLE DeliveryMan ADD CONSTRAINT FKDeliveryMa443268 FOREIGN KEY (DeliveryID) REFERENCES Delivery (ID);
-ALTER TABLE Staff ADD CONSTRAINT FKStaff2713 FOREIGN KEY (StoreCode) REFERENCES Store (Code);
-ALTER TABLE Administrator ADD CONSTRAINT FKAdministra475433 FOREIGN KEY (StoreCode) REFERENCES Store (Code);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deliveryman`
+--
+
+CREATE TABLE `deliveryman` (
+  `DeliveryID` int(10) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Phone` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `Code` int(10) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `ItemDescription` varchar(255) NOT NULL,
+  `QtyInStock` int(10) NOT NULL,
+  `BuyPrice` decimal(19,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `ItemCode` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `ID` int(10) NOT NULL,
+  `CustomerID` int(10) NOT NULL,
+  `OrderTime` time(6) NOT NULL,
+  `OrderDate` date NOT NULL,
+  `Shopping_CartOrder_ItemID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item`
+--
+
+CREATE TABLE `order_item` (
+  `ID` int(10) NOT NULL,
+  `ItemCode` int(10) NOT NULL,
+  `Qty` int(10) NOT NULL,
+  `PriceEach` decimal(19,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `OrderQueue` int(10) NOT NULL,
+  `OrderID` int(10) NOT NULL,
+  `OrderStatus` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `CheckNum` varchar(255) NOT NULL,
+  `CustomerID` int(10) NOT NULL,
+  `PaymentDate` date NOT NULL,
+  `Amount` decimal(19,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt`
+--
+
+CREATE TABLE `receipt` (
+  `Receipt` int(10) NOT NULL,
+  `CustomerID` int(10) NOT NULL,
+  `PaymentCheckNum` varchar(255) NOT NULL,
+  `OrderID` int(10) NOT NULL,
+  `GenerateDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `ID` int(10) NOT NULL,
+  `CustomerID` int(10) NOT NULL,
+  `Comment` varchar(1000) NOT NULL,
+  `Rating` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopping_cart`
+--
+
+CREATE TABLE `shopping_cart` (
+  `Order_ItemID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `JobTitle` varchar(100) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `Order_ListOrderQueue` int(10) NOT NULL,
+  `StoreCode` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `Code` int(10) NOT NULL,
+  `City` varchar(255) NOT NULL,
+  `Phone` int(11) NOT NULL,
+  `Address1` varchar(255) NOT NULL,
+  `Address2` varchar(255) NOT NULL,
+  `State` varchar(255) NOT NULL,
+  `Country` varchar(255) NOT NULL,
+  `PostalCode` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKAdmin` (`StoreCode`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKDelivery` (`Order_ListOrderQueue`);
+
+--
+-- Indexes for table `deliveryman`
+--
+ALTER TABLE `deliveryman`
+  ADD KEY `FKDeliveryMan` (`DeliveryID`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`Code`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD KEY `FK_Menu` (`ItemCode`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD KEY `FK_Order` (`CustomerID`);
+
+--
+-- Indexes for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKOrder_Item` (`ItemCode`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD PRIMARY KEY (`OrderQueue`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`CheckNum`),
+  ADD KEY `FKPayment` (`CustomerID`);
+
+--
+-- Indexes for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD PRIMARY KEY (`Receipt`),
+  ADD KEY `FKReceipt` (`CustomerID`),
+  ADD KEY `FK_Receipt_Payment` (`PaymentCheckNum`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKReview` (`CustomerID`);
+
+--
+-- Indexes for table `shopping_cart`
+--
+ALTER TABLE `shopping_cart`
+  ADD PRIMARY KEY (`Order_ItemID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKStaff` (`Order_ListOrderQueue`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`Code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `Code` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_list`
+--
+ALTER TABLE `order_list`
+  MODIFY `OrderQueue` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `receipt`
+--
+ALTER TABLE `receipt`
+  MODIFY `Receipt` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+  MODIFY `Code` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD CONSTRAINT `FKAdmin` FOREIGN KEY (`StoreCode`) REFERENCES `store` (`Code`);
+
+--
+-- Constraints for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD CONSTRAINT `FKDelivery` FOREIGN KEY (`Order_ListOrderQueue`) REFERENCES `order_list` (`OrderQueue`);
+
+--
+-- Constraints for table `deliveryman`
+--
+ALTER TABLE `deliveryman`
+  ADD CONSTRAINT `FKDeliveryMan` FOREIGN KEY (`DeliveryID`) REFERENCES `delivery` (`ID`);
+
+--
+-- Constraints for table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `FK_Menu` FOREIGN KEY (`ItemCode`) REFERENCES `item` (`Code`);
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `FK_Order` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`ID`);
+
+--
+-- Constraints for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `FKOrder_Item` FOREIGN KEY (`ItemCode`) REFERENCES `item` (`Code`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `FKPayment` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`ID`);
+
+--
+-- Constraints for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD CONSTRAINT `FKReceipt` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`ID`),
+  ADD CONSTRAINT `FK_Receipt_Payment` FOREIGN KEY (`PaymentCheckNum`) REFERENCES `payment` (`CheckNum`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `FKReview` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`ID`);
+
+--
+-- Constraints for table `shopping_cart`
+--
+ALTER TABLE `shopping_cart`
+  ADD CONSTRAINT `FKShopping` FOREIGN KEY (`Order_ItemID`) REFERENCES `order_item` (`ID`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `FKStaff` FOREIGN KEY (`Order_ListOrderQueue`) REFERENCES `order_list` (`OrderQueue`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
