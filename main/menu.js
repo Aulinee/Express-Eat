1e691b16253f2dd7ee06af851cf7d1eded5854b6
Aulinee <audreyduyan1@gmail.com>
2021-01-13 13:57:48 +0800

    finish js for menu

--- main/menu.js
+++ main/menu.js
@@ -219,3 +219,272 @@ function incrementcake6() {
 function decrementcake6() {
   document.getElementById('tiramisu').stepDown();
 }
+
+//Cannoli
+function incrementcake7() {
+  document.getElementById('cannoli').stepUp();
+}
+function decrementcake7() {
+  document.getElementById('cannoli').stepDown();
+}
+
+//Churros
+function incrementcake8() {
+  document.getElementById('churros').stepUp();
+}
+function decrementcake8() {
+  document.getElementById('churros').stepDown();
+}
+
+//Cinnammon Rolls
+function incrementcake9() {
+  document.getElementById('cinnamon').stepUp();
+}
+function decrementcake9() {
+  document.getElementById('cinnamon').stepDown();
+}
+
+//Curry Puff
+function incrementcake10() {
+  document.getElementById('currypuff').stepUp();
+}
+
+function decrementcake10() {
+  document.getElementById('currypuff').stepDown();
+}
+
+//Macaron
+function incrementcake11() {
+  document.getElementById('macaron').stepUp();
+}
+
+function decrementcake11() {
+  document.getElementById('macaron').stepDown();
+}
+
+//Pretzel
+function incrementcake12() {
+  document.getElementById('pretzel').stepUp();
+}
+
+function decrementcake12() {
+  document.getElementById('pretzel').stepDown();
+}
+
+//Classic Waffle
+function incrementWaffle1() {
+  document.getElementById('classicwaffle').stepUp();
+}
+
+function decrementWaffle1() {
+  document.getElementById('classicwaffle').stepDown();
+}
+
+//Belgian Waffle Syrup
+function incrementWaffle2() {
+  document.getElementById('wafflesyrup').stepUp();
+}
+
+function decrementWaffle2() {
+  document.getElementById('wafflesyrup').stepDown();
+}
+
+//Belgian Waffle Banana
+function incrementWaffle3() {
+  document.getElementById('wafflebanana').stepUp();
+}
+
+function decrementWaffle3() {
+  document.getElementById('wafflebanana').stepDown();
+}
+
+//Belgian Waffle Oreo
+function incrementWaffle4() {
+  document.getElementById('waffleoreo').stepUp();
+}
+
+function decrementWaffle4() {
+  document.getElementById('waffleoreo').stepDown();
+}
+
+//Belgian Waffle Strawberry
+function incrementWaffle5() {
+  document.getElementById('wafflestrawberry').stepUp();
+}
+
+function decrementWaffle5() {
+  document.getElementById('wafflestrawberry').stepDown();
+}
+
+//Belgian Waffle Nutella
+function incrementWaffle6() {
+  document.getElementById('wafflenutella').stepUp();
+}
+
+function decrementWaffle6() {
+  document.getElementById('wafflenutella').stepDown();
+}
+
+//Aglio Olio
+function incrementPasta1() {
+  document.getElementById('aglio').stepUp();
+}
+
+function decrementPasta1() {
+  document.getElementById('aglio').stepDown();
+}
+
+//Carbonara
+function incrementPasta2() {
+  document.getElementById('carbonara').stepUp();
+}
+
+function decrementPasta2() {
+  document.getElementById('carbonara').stepDown();
+}
+
+//Spaghetti Bolognese
+function incrementPasta3() {
+  document.getElementById('bolognese').stepUp();
+}
+
+function decrementPasta3() {
+  document.getElementById('bolognese').stepDown();
+}
+
+//Pepperoni pizza
+function incrementPizza1() {
+  document.getElementById('pepperoni').stepUp();
+}
+
+function decrementPizza1() {
+  document.getElementById('pepperoni').stepDown();
+}
+
+//Hawaiian pizza
+function incrementPizza2() {
+  document.getElementById('hawaiian').stepUp();
+}
+
+function decrementPizza2() {
+  document.getElementById('hawaiian').stepDown();
+}
+
+//Mix pizza
+function incrementPizza3() {
+  document.getElementById('mix').stepUp();
+}
+
+function decrementPizza3() {
+  document.getElementById('mix').stepDown();
+}
+
+//Classic Salad
+function incrementSalad1() {
+  document.getElementById('classicsalad').stepUp();
+}
+
+function decrementSalad1() {
+  document.getElementById('classicsalad').stepDown();
+}
+
+//Chicken Salad
+function incrementSalad2() {
+  document.getElementById('chickensalad').stepUp();
+}
+
+function decrementSalad2() {
+  document.getElementById('chickensalad').stepDown();
+}
+
+//Seafood Salad
+function incrementSalad3() {
+  document.getElementById('seafoodsalad').stepUp();
+}
+
+function decrementSalad3() {
+  document.getElementById('seafoodsalad').stepDown();
+}
+
+//Fruit Salad
+function incrementSalad4() {
+  document.getElementById('fruitsalad').stepUp();
+}
+
+function decrementSalad4() {
+  document.getElementById('fruitsalad').stepDown();
+}
+
+//Chicken Lasagna
+function incrementSide1() {
+  document.getElementById('chickenlasagna').stepUp();
+}
+
+function decrementSide1() {
+  document.getElementById('chickenlasagna').stepDown();
+}
+
+//Beef Lasagna
+function incrementSide2() {
+  document.getElementById('beeflasagna').stepUp();
+}
+
+function decrementSide2() {
+  document.getElementById('beeflasagna').stepDown();
+}
+
+//Chicken Wing
+function incrementSide3() {
+  document.getElementById('chickenwings').stepUp();
+}
+
+function decrementSide3() {
+  document.getElementById('chickenwings').stepDown();
+}
+
+//Sandwich
+function incrementSide4() {
+  document.getElementById('sandwich').stepUp();
+}
+
+function decrementSide4() {
+  document.getElementById('sandwich').stepDown();
+}
+
+//French fries
+function incrementSide5() {
+  document.getElementById('frenchfries').stepUp();
+}
+
+function decrementSide5() {
+  document.getElementById('frenchfries').stepDown();
+}
+
+//Mashed Potato
+function incrementSide6() {
+  document.getElementById('mashedpotato').stepUp();
+}
+
+function decrementSide6() {
+  document.getElementById('mashedpotato').stepDown();
+}
+
+//Mac & Cheese
+function incrementSide7() {
+  document.getElementById('macncheese').stepUp();
+}
+
+function decrementSide7() {
+  document.getElementById('macncheese').stepDown();
+}
+
+//Mushroom Soup
+function incrementSide8() {
+  document.getElementById('mushroom').stepUp();
+}
+
+function decrementSide8() {
+  document.getElementById('mushroom').stepDown();
+}
+
+
